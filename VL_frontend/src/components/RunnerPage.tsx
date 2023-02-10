9804155ebcfdd9beb87aa12f17cd794899dd6c87
Otso Westerlund <otsowesterlund@gmail.com>
2023-02-10 12:32:34 +0200

    made RunnerPage code cleaner

--- VL_frontend/src/components/RunnerPage.tsx
+++ VL_frontend/src/components/RunnerPage.tsx
@@ -8,78 +8,37 @@ const RunnerPage = () => {
   const allRunners = useRecoilValue(allRunnersState);
   const order = useRecoilValue(runnerOrderState);
 
+  const sortedRunners = [...allRunners];
   if (order === 'points') {
-    return (
-      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
-        {[...allRunners]
-          .sort((a, b) => b.points - a.points)
-          .map((runner) => (
-            <RunnerCard
-              key={runner.id}
-              blur={runner.selected ? true : false}
-              runner={runner.name}
-              runnerPhoto={runner.runnerPhoto}
-              price={runner.price}
-              points={runner.points}
-              team={runner.team}
-              teamPhoto={`${runner.team}.png`}
-              id={runner.id}
-              displayDelete={false}
-              dbRunners={allRunners}
-            />
-          ))}
-      </Grid>
-    );
-  }
-  if (order === 'price') {
-    return (
-      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
-        {[...allRunners]
-          .sort((a, b) => b.price - a.price)
-          .map((runner) => (
-            <RunnerCard
-              key={runner.id}
-              blur={runner.selected ? true : false}
-              runner={runner.name}
-              runnerPhoto={runner.runnerPhoto}
-              price={runner.price}
-              points={runner.points}
-              team={runner.team}
-              teamPhoto={`${runner.team}.png`}
-              id={runner.id}
-              displayDelete={false}
-              dbRunners={allRunners}
-            />
-          ))}
-      </Grid>
-    );
+    sortedRunners.sort((a, b) => b.points - a.points);
+  } else if (order === 'price') {
+    sortedRunners.sort((a, b) => b.price - a.price);
   } else {
-    return (
-      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
-        {[...allRunners]
-          .sort((a, b) => {
-            const [_firstNameA, lastNameA] = a.name.split(' ');
-            const [_firstNameB, lastNameB] = b.name.split(' ');
-            return lastNameA.localeCompare(lastNameB);
-          })
-          .map((runner) => (
-            <RunnerCard
-              key={runner.id}
-              blur={runner.selected ? true : false}
-              runner={runner.name}
-              runnerPhoto={runner.runnerPhoto}
-              price={runner.price}
-              points={runner.points}
-              team={runner.team}
-              teamPhoto={`${runner.team}.png`}
-              id={runner.id}
-              displayDelete={false}
-              dbRunners={allRunners}
-            />
-          ))}
-      </Grid>
-    );
+    sortedRunners.sort((a, b) => {
+      const [_firstNameA, lastNameA] = a.name.split(' ');
+      const [_firstNameB, lastNameB] = b.name.split(' ');
+      return lastNameA.localeCompare(lastNameB);
+    });
   }
+  return (
+    <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+      {sortedRunners.map((runner) => (
+        <RunnerCard
+          key={runner.id}
+          blur={runner.selected ? true : false}
+          runner={runner.name}
+          runnerPhoto={runner.runnerPhoto}
+          price={runner.price}
+          points={runner.points}
+          team={runner.team}
+          teamPhoto={`${runner.team}.png`}
+          id={runner.id}
+          displayDelete={false}
+          dbRunners={allRunners}
+        />
+      ))}
+    </Grid>
+  );
 };
 
 export default RunnerPage;
